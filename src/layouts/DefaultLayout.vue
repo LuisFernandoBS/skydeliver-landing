f42ac859e1fbd2df82326002e83c0f5ff716c5ca
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-02 02:09:14 -0300

    Animação de entrada do menu

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="min-h-screen min-w-screen flex flex-col bg-fundo dark:bg-fundo-dark">
+  <div class="min-h-screen min-w-screen max-w-screen flex flex-col bg-fundo dark:bg-fundo-dark">
 
-    <header class="grid grid-cols-1 md:grid-cols-10 shadow-md">
-        <div class="col-span-1 col-start-1 md:col-span-6 md:col-start-3">
-            <nav class="grid grid-cols-1 md:grid-cols-6 items-center gap-4 py-7">
-                <div class="col-span-1 col-start-1">
-                    <h1 class="text-xl font-montserrat-bold text-primaria dark:text-primaria-dark">SkyDeliver</h1>
+    <header class="grid grid-cols-1 md:grid-cols-10">
+      <img src="../assets/images/drone-menu.png" alt="drone menu" class="w-16 h-16 md:w-22 md:h-22 drone-menu fixed md:left-5 2xl:left-40" />
+        <div class="div-nav col-span-1 col-start-1 md:col-span-8 2xl:col-span-6 md:col-start-2 2xl:col-start-3">
+            <nav class="grid grid-cols-1 md:grid-cols-4 2xl:grid-cols-6 items-center md:gap-2 2xl:gap-4 py-7">
+                <div class="col-span-1 col-start-1 anim-col delay-1">
+                    <h1 class="text-2xl font-montserrat-bold text-primaria dark:text-primaria-dark">SkyDeliver</h1>
                 </div>
-                <div class="col-span-1 col-start-1 md:col-start-4 text-center">
-                    <a class="cursor-pointer text-xl font-montserrat text-primaria dark:text-primaria-dark hover:font-montserrat-bold">Início</a>
+                <div class="col-span-1 col-start-1 anim-col md:col-start-2 2xl:col-start-4 text-center delay-2">
+                    <a class="cursor-pointer md:text-md xl:text-xl font-montserrat text-primaria dark:text-primaria-dark hover:font-montserrat-bold">Início</a>
                 </div>
-                <div class="col-span-1 text-center">
-                    <a class="cursor-pointer text-xl font-montserrat text-primaria dark:text-primaria-dark hover:font-montserrat-bold">Planos e Preços</a>
+                <div class="col-span-1 text-center anim-col delay-3">
+                    <a class="cursor-pointer md:text-md xl:text-xl font-montserrat text-primaria dark:text-primaria-dark hover:font-montserrat-bold">Planos e Preços</a>
                 </div>
-                <div class="col-span-1 text-center">
-                    <a class="cursor-pointer text-xl font-montserrat text-primaria dark:text-primaria-dark hover:font-montserrat-bold">Contato</a>
+                <div class="col-span-1 text-center anim-col delay-4">
+                    <a class="cursor-pointer md:text-md xl:text-xl font-montserrat text-primaria dark:text-primaria-dark hover:font-montserrat-bold">Contato</a>
                 </div>
             </nav>
         </div>
@@ -34,4 +35,87 @@
 </script>
 
 <style scoped>
+  @keyframes fadeInLeft {
+    0% {
+      opacity: 0;
+      transform: translateX(-30px);
+    }
+    100% {
+      opacity: 1;
+      visibility: visible;
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes bounceInLeft {
+    0% {
+      opacity: 0;
+      transform: translateX(-1000px);
+    }
+    60% {
+      opacity: 1;
+      transform: translateX(30px);
+    }
+    80% { transform: translateX(-20px); }
+    100% { transform: translateX(0); }
+  }
+
+  @keyframes fadeOutLeft {
+    0% {
+      opacity: 0;
+      transform: translateX(-1000px);
+    }
+    60% {
+      opacity: 1;
+      transform: translateX(30px);
+    }
+    100% { transform: translateX(0); }
+  }
+
+  @keyframes fadeOutRight {
+    0% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+    95% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+      transform: translateX(100vw);
+      visibility: hidden;
+    }
+  }
+
+  .anim-col {
+    opacity: 0;
+    visibility: hidden;
+    animation: fadeInLeft 1.8s ease-out forwards;
+  }
+
+  .drone-menu {
+    animation: bounceInLeft 3s ease forwards, fadeOutRight 4s ease 3s forwards;
+  }
+
+  
+  .delay-1 { animation-delay: 3.4s; }
+  .delay-2 { animation-delay: 4s; }
+  .delay-3 { animation-delay: 4.3s; }
+  .delay-4 { animation-delay: 4.5s; }
+  
+  @media (max-width: 1260px) {
+    .delay-1 { animation-delay: 3.3s; }
+    .delay-2 { animation-delay: 3.8s; }
+    .delay-3 { animation-delay: 4.2s; }
+    .delay-4 { animation-delay: 4.5s; }
+  }
+
+  @media (min-width: 1261px) and (max-width: 1450px) {
+    .delay-1 { animation-delay: 3.3s; }
+    .delay-2 { animation-delay: 3.8s; }
+    .delay-3 { animation-delay: 4.2s; }
+    .delay-4 { animation-delay: 4.8s; }
+  }
+
+
 </style>
