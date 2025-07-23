11b5d1b47d304bc7d7b25151b866fb1162d7dbca
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-22 21:01:44 -0300

    Implementado componente CampoForm no formulario de contatos

--- src/pages/ContatoPage.vue
+++ src/pages/ContatoPage.vue
@@ -21,73 +21,31 @@
         </div>
         <div class="col-span-3 mt-8">
             <h2 class="text-xl text-texto text-shadow-texto font-opensans-bold mb-4">Formulário de Contato</h2>
-            <form class="flex flex-col space-y-4">
-                <label for="nomeEmpresa" class="relative">
-                    <input
-                    type="text"
-                    id="nomeEmpresa"
-                    placeholder=""
-                    class="peer mt-0.5 w-full py-4 rounded border-gray-300 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
-                    />
-                    <span
-                    class="absolute inset-y-0 start-3 -translate-y-5 mt-2 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5 dark:bg-gray-900 dark:text-white"
-                    >
-                    Nome da empresa
-                    </span>
-                </label>
-                <label for="cnpj" class="relative">
-                    <input
-                    type="text"
-                    id="cnpj"
-                    placeholder=""
-                    class="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
-                    />
-                    <span
-                    class="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5 dark:bg-gray-900 dark:text-white"
-                    >
-                    CNPJ
-                    </span>
-                </label>
-                <label for="EmailCorporativo" class="relative">
-                    <input
-                    type="email"
-                    id="EmailCorporativo"
-                    placeholder=""
-                    class="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
-                    />
-                    <span
-                    class="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5 dark:bg-gray-900 dark:text-white"
-                    >
-                    Email
-                    </span>
-                </label>
-                <label for="numEstimadoEntrega" class="relative">
-                    <input
-                    type="number"
-                    id="numEstimadoEntrega"
-                    placeholder=""
-                    class="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
-                    />
-                    <span
-                    class="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5 dark:bg-gray-900 dark:text-white"
-                    >
-                    Numero estimado de entregas/mês
-                    </span>
-                </label>
-                <label for="mensagem">
-                    <span class="text-sm font-medium text-gray-700"> Mensagem </span>
-                
+            <form @submit.prevent="enviarFormulario" class="flex flex-col space-y-4">
+                <CampoForm id="nomeEmpresa" type="text" label="Nome da empresa" />
+                <CampoForm id="cnpj" type="text" label="CNPJ" />
+                <CampoForm id="emailCorporativo" type="text" label="Email" />
+                <CampoForm id="numEstimadoEntrega" type="number" label="Numero estimado de entregas/mês" />
+                <label for="mensagem" class="relative">
                     <textarea
                     id="mensagem"
-                    class="mt-0.5 w-full resize-none rounded border-gray-300 shadow-sm sm:text-sm"
+                    class="peer mt-0.5 w-full rounded text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm"
                     rows="4"
                     ></textarea>
+
+                    <span class="absolute inset-y-0 start-3 -translate-y-[-3px] h-[80%] bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-3"> Mensagem </span>
                 </label>
             </form>
         </div>
     </div>
 </template>
 <script setup>
+    import CampoForm from './Contato/CampoForm.vue';
+
+    function enviarFormulario(){
+        const campos = document.querySelectorAll('input, textarea');
+        console.log(campos);
+    }
 </script>
 <style>
     
