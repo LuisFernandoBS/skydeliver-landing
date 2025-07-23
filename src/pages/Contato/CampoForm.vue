9ff52b4166d591d6e2046330bd49e712e556c87c
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-22 21:00:56 -0300

    Componete input padrão para o form de contatos

---
+++ src/pages/Contato/CampoForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <label :for="id" class="relative">
+            <input
+            :type="type"
+            :id="id"
+            placeholder=""
+            class="peer mt-0.5 w-full rounded py-4 text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm dark:border-gray-600"
+            />
+            <span
+            class="absolute inset-y-0 start-3 -translate-y-2 bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6"
+            >
+            {{ label }}
+            </span>
+        </label>
+    </div>
+</template>
+<script>
+export default {
+     props: {
+        type: String,
+        id: String,
+        label: String,
+    }
+}
+</script>
+<style>
+    
+</style>
