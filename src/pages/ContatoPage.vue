bd751738658aa1c43a50d792f47d0ff437bb0a17
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-23 23:52:49 -0300

    Implementada imagem ao formulario de contato

--- src/pages/ContatoPage.vue
+++ src/pages/ContatoPage.vue
@@ -20,22 +20,29 @@
             <p class="text-texto text-shadow-texto font-montserrat">contato@skyline.com</p>
         </div>
         <div class="col-span-3 mt-8">
-            <h2 class="text-xl text-texto text-shadow-texto font-opensans-bold mb-4">Formulário de Contato</h2>
-            <form @submit.prevent="enviarFormulario" class="flex flex-col space-y-4">
-                <CampoForm id="nomeEmpresa" type="text" label="Nome da empresa" />
-                <CampoForm id="cnpj" type="text" label="CNPJ" />
-                <CampoForm id="emailCorporativo" type="text" label="Email" />
-                <CampoForm id="numEstimadoEntrega" type="number" label="Numero estimado de entregas/mês" />
-                <label for="mensagem" class="relative">
-                    <textarea
-                    id="mensagem"
-                    class="peer mt-0.5 w-full rounded text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm"
-                    rows="4"
-                    ></textarea>
-
-                    <span class="absolute inset-y-0 start-3 -translate-y-[-3px] h-[80%] bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-3"> Mensagem </span>
-                </label>
-            </form>
+            <div class="flex flex-wrap w-[80vw] xl:w-[85vw] 2xl:w-[80vw]">
+                <div class="w-screen lg:w-1/2 pr-0 py-0 lg:pr-10 lg:py-6">
+                    <img class="w-[95%] h-[95%] mx-[2.5%] object-contain object-center" src="@/assets/images/contato.jpg" alt="contato">
+                </div>
+                <div class="w-screen lg:w-1/2 pr-0 py-0 lg:pr-10 lg:py-6">
+                    <h2 class="text-xl text-texto text-shadow-texto font-opensans-bold mb-4">Formulário de Contato</h2>
+                    <form @submit.prevent="enviarFormulario" class="flex flex-col space-y-4">
+                        <CampoForm id="nomeEmpresa" type="text" label="Nome da empresa" />
+                        <CampoForm id="cnpj" type="text" label="CNPJ" />
+                        <CampoForm id="emailCorporativo" type="text" label="Email" />
+                        <CampoForm id="numEstimadoEntrega" type="number" label="Numero estimado de entregas/mês" />
+                        <label for="mensagem" class="relative">
+                            <textarea
+                            id="mensagem"
+                            class="peer mt-0.5 w-full rounded text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm"
+                            rows="4"
+                            ></textarea>
+        
+                            <span class="absolute inset-y-0 start-3 -translate-y-[-3px] h-[80%] bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-3"> Mensagem </span>
+                        </label>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
