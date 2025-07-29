ae0ff07a2d98024cf6e281ff5510a9d09911c51e
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-29 01:44:26 -0300

    Criada pagina de planos

---
+++ src/pages/PlanosPage.vue
@@ -0,0 +1,221 @@
+<template>
+    <div class="grid grid-cols-3 gap-4 mt-0 md:mt-[10vh] px-[5vw] md:px-[10vw] xl:px-[20vw]">
+        <div class="col-span-3 text-center mb-3 sm:mb-8 animate-[fadeIn_200ms_ease-out_forwards]">
+            <h1 class="text-2xl text-texto text-shadow-texto font-opensans-bold mb-4">Planos e Preços</h1>
+            <p class="text-texto text-shadow-texto mb-2 sm:mb-6 font-montserrat">
+                Escolha o plano ideal para o seu negócio e comece a transformar suas entregas hoje mesmo.
+            </p>
+        </div>
+        <div class="col-span-3">
+            <div class="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
+                    <div class="rounded-2xl border border-indigo-600 p-6 shadow-xs ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12">
+                        <div class="text-center">
+                            <h2 class="text-lg font-medium text-texto">
+                                Pro
+                                <span class="sr-only">Plan</span>
+                            </h2>
+                    
+                            <p class="mt-2 sm:mt-4">
+                                <strong class="text-3xl font-bold text-texto sm:text-4xl"> R$ 300 </strong>
+                    
+                                <span class="text-sm font-medium text-gray-500">/mês</span>
+                            </p>
+                        </div>
+                
+                        <ul class="mt-6 space-y-2">
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> 20 users included </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> 5GB of storage </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> Email support </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> Help center access </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> Phone support </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> Community access </span>
+                            </li>
+                        </ul>
+                
+                        <a
+                        href="#"
+                        class="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:ring-3 focus:outline-hidden"
+                        >
+                        Solicitar proposta
+                        </a>
+                    </div>
+                
+                    <div class="rounded-2xl border border-texto p-6 shadow-xs sm:px-8 lg:p-12">
+                        <div class="text-center">
+                            <h2 class="text-lg font-medium text-texto">
+                                Starter
+                                <span class="sr-only">Plan</span>
+                            </h2>
+                    
+                            <p class="mt-2 sm:mt-4">
+                                <strong class="text-3xl font-bold text-texto sm:text-4xl"> R$ 200 </strong>
+                    
+                                <span class="text-sm font-medium text-gray-500">/mês</span>
+                            </p>
+                        </div>
+                
+                        <ul class="mt-6 space-y-2">
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> 10 users included </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> 2GB of storage </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> Email support </span>
+                            </li>
+                    
+                            <li class="flex items-center gap-1">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-5 text-indigo-700 shadow-sm"
+                                >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                    
+                                <span class="text-gray-500"> Help center access </span>
+                            </li>
+                        </ul>
+                
+                        <a
+                        href="#"
+                        class="mt-8 block rounded-full border border-indigo-600 dark:border-texto bg-fundo px-12 py-3 text-center text-sm font-medium text-indigo-600 dark:text-texto hover:ring-1 hover:ring-texto focus:ring-3 focus:outline-hidden"
+                        >
+                        Solicitar proposta
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
