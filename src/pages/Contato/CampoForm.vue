0f7507fe5e2dbcd8a8ba6720254c9699fcb8028b
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-28 21:37:54 -0300

    Ajustado posição da label no campo após preenchido.

--- src/pages/Contato/CampoForm.vue
+++ src/pages/Contato/CampoForm.vue
@@ -5,10 +5,10 @@
             :type="type"
             :id="id"
             placeholder=""
-            class="peer mt-0.5 w-full rounded py-4 text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm dark:border-gray-600"
+            class="peer mt-0.5 w-full rounded py-4 px-4 text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm dark:border-gray-600"
             />
             <span
-            class="absolute inset-y-0 start-3 -translate-y-2 bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6"
+            class="absolute inset-y-0 start-3 -translate-y-6 bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:-translate-y-0 peer-focus:-translate-y-6"
             >
             {{ label }}
             </span>
