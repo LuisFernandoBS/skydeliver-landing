3a8b766c7733822162937e56ac8bb0797c219c7a
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-28 22:20:53 -0300

    Acresentado parâmetro required no componente CamposForm

--- src/pages/Contato/CampoForm.vue
+++ src/pages/Contato/CampoForm.vue
@@ -5,12 +5,14 @@
             :type="type"
             :id="id"
             placeholder=""
+            :required="required"
+            :name="label"
             class="peer mt-0.5 w-full rounded py-4 px-4 text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm dark:border-gray-600"
             />
             <span
             class="absolute inset-y-0 start-3 -translate-y-6 bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:-translate-y-0 peer-focus:-translate-y-6"
             >
-            {{ label }}
+            {{ label }} {{ required ? '*' : '' }}
             </span>
         </label>
     </div>
@@ -21,6 +23,10 @@ export default {
         type: String,
         id: String,
         label: String,
+        required: {
+            type: Boolean,
+            default: false
+        }
     }
 }
 </script>
