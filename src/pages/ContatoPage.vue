01303d825e09d1331b5c4e9afb9c447141747ff2
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-28 21:52:49 -0300

    Ajustado distanciamento dos campos no form de contato

--- src/pages/ContatoPage.vue
+++ src/pages/ContatoPage.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="grid grid-cols-3 gap-4 mt-[8vh] px-[20vw]">
-        <div class="col-span-3 text-center mb-8">
+    <div class="grid grid-cols-3 gap-4 mt-[10vh] px-[20vw]">
+        <div class="col-span-3 text-center mb-8 animate-[fadeIn_1s_ease-out_forwards]">
             <h1 class="text-2xl text-texto text-shadow-texto font-opensans-bold mb-4">Contato</h1>
             <p class="text-texto text-shadow-texto mb-6 font-montserrat">Entre em contato conosco para mais informações.</p>
         </div>
-        <div class="col-span-1 flex flex-col items-center justify-center">
+        <div class="col-span-1 flex flex-col items-center justify-center animate-[fadeIn_2s_ease-out_forwards]">
             <img src="@/assets/images/telefone.png" alt="telefone" class="w-17 h-17 mb-4">
             <h4 class="text-lg font-bold font-opensans text-texto">Telefone</h4>
             <p class="text-texto text-shadow-texto font-montserrat">+55 11 1234-5678</p>
         </div>
-        <div class="col-span-1 flex flex-col items-center justify-center">
+        <div class="col-span-1 flex flex-col items-center justify-center animate-[fadeIn_2s_ease-out_forwards]">
             <img src="@/assets/images/localizacao.png" alt="localização" class="w-17 h-17 mb-4">
             <h4 class="text-lg font-bold font-opensans text-texto">Endereço</h4>
             <p class="text-texto text-shadow-texto font-montserrat">Av. Oceânica, 456 - Barra, Salvador, BA</p>
         </div>
-        <div class="col-span-1 flex flex-col items-center justify-center">
+        <div class="col-span-1 flex flex-col items-center justify-center animate-[fadeIn_2s_ease-out_forwards]">
             <img src="@/assets/images/email.png" alt="email" class="w-17 h-17 mb-4">
             <h4 class="text-lg font-bold font-opensans text-texto">Email</h4>
             <p class="text-texto text-shadow-texto font-montserrat">contato@skyline.com</p>
         </div>
-        <div class="col-span-3 mt-8">
-            <div class="flex flex-wrap w-[80vw] xl:w-[85vw] 2xl:w-[80vw]">
-                <div class="w-screen lg:w-1/2 pr-0 py-0 lg:pr-10 lg:py-6">
-                    <img class="w-[95%] h-[95%] mx-[2.5%] object-contain object-center" src="@/assets/images/contato.jpg" alt="contato">
+        <div class="col-span-3 mt-8 animate-[fadeIn_2s_ease-out_forwards]">
+            <div class="flex flex-wrap w-[100%] border rounded-2xl my-0 lg:my-6">
+                <div class="w-screen lg:w-[50%] pr-5">
+                    <img class="object-contain object-center border rounded-2xl" src="@/assets/images/contato.jpg" alt="contato">
                 </div>
-                <div class="w-screen lg:w-1/2 pr-0 py-0 lg:pr-10 lg:py-6">
-                    <h2 class="text-xl text-texto text-shadow-texto font-opensans-bold mb-4">Formulário de Contato</h2>
-                    <form @submit.prevent="enviarFormulario" class="flex flex-col space-y-4">
+                <div class="w-screen lg:w-[50%] pr-0">
+                    <h2 class="text-xl text-texto text-shadow-texto font-opensans-bold my-4 px-8">Formulário de Contato</h2>
+                    <form @submit.prevent="enviarFormulario" class="flex flex-col space-y-4 px-5">
                         <CampoForm id="nomeEmpresa" type="text" label="Nome da empresa" />
                         <CampoForm id="cnpj" type="text" label="CNPJ" />
                         <CampoForm id="emailCorporativo" type="text" label="Email" />
@@ -34,12 +34,35 @@
                         <label for="mensagem" class="relative">
                             <textarea
                             id="mensagem"
-                            class="peer mt-0.5 w-full rounded text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm"
-                            rows="4"
-                            ></textarea>
+                            class="peer mt-0.5 pt-3 px-4 w-full rounded text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm"
+                            placeholder=""
+                            rows="4"></textarea>
         
-                            <span class="absolute inset-y-0 start-3 -translate-y-[-3px] h-[80%] bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-3"> Mensagem </span>
+                            <span class="absolute inset-y-0 start-3 -translate-y-4 mt-2 h-fit bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:-translate-y-0 peer-focus:-translate-y-4"> Mensagem </span>
                         </label>
+                        <button
+                        type="submit"
+                        class="group relative w-[25%] ml-auto cursor-pointer inline-flex items-center overflow-hidden rounded-sm bg-primaria px-8 py-3 text-texto focus:outline-hidden"
+                        >
+                            <span class="absolute -end-full transition-all group-hover:end-4">
+                                <svg
+                                class="size-5 shadow-sm rtl:rotate-180"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                />
+                                </svg>
+                            </span>
+
+                            <span class="text-sm font-medium transition-all font-montserrat-bold group-hover:me-4"> Enviar </span>
+                        </button>
                     </form>
                 </div>
             </div>
