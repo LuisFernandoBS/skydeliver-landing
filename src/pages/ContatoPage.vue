fc88c0282ad705be24ca6aa5a1fb280bc5b75726
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-28 22:22:04 -0300

    Captura e validação para campos vazios no formulario de contato

--- src/pages/ContatoPage.vue
+++ src/pages/ContatoPage.vue
@@ -20,49 +20,56 @@
             <p class="text-texto text-shadow-texto font-montserrat">contato@skyline.com</p>
         </div>
         <div class="col-span-3 mt-8 animate-[fadeIn_2s_ease-out_forwards]">
-            <div class="flex flex-wrap w-[100%] border rounded-2xl my-0 lg:my-6">
-                <div class="w-screen lg:w-[50%] pr-5">
-                    <img class="object-contain object-center border rounded-2xl" src="@/assets/images/contato.jpg" alt="contato">
+            <div class="flex flex-wrap w-[100%] border border-secundaria rounded-2xl my-0 lg:my-6">
+                <div class="w-screen lg:w-[50%]">
+                    <img class="object-cover object-center border border-secundaria rounded-2xl h-full" src="@/assets/images/contato.jpg" alt="contato">
                 </div>
                 <div class="w-screen lg:w-[50%] pr-0">
-                    <h2 class="text-xl text-texto text-shadow-texto font-opensans-bold my-4 px-8">Formulário de Contato</h2>
+                    <h2 class="text-xl text-texto text-shadow-texto font-opensans-bold my-6 px-8">Formulário de Contato</h2>
                     <form @submit.prevent="enviarFormulario" class="flex flex-col space-y-4 px-5">
-                        <CampoForm id="nomeEmpresa" type="text" label="Nome da empresa" />
-                        <CampoForm id="cnpj" type="text" label="CNPJ" />
-                        <CampoForm id="emailCorporativo" type="text" label="Email" />
+                        <CampoForm id="nomeEmpresa" type="text" label="Nome da empresa" :required="true"/>
+                        <CampoForm id="cnpj" type="text" label="CNPJ" :required="true"/>
+                        <CampoForm id="emailCorporativo" type="text" label="Email" :required="true"/>
                         <CampoForm id="numEstimadoEntrega" type="number" label="Numero estimado de entregas/mês" />
                         <label for="mensagem" class="relative">
                             <textarea
                             id="mensagem"
                             class="peer mt-0.5 pt-3 px-4 w-full rounded text-texto border-gray-300 bg-fundo shadow-sm sm:text-sm"
                             placeholder=""
-                            rows="4"></textarea>
+                            rows="4"
+                            name="Mensagem"
+                            minlength="10"
+                            maxlength="400"
+                            required></textarea>
         
                             <span class="absolute inset-y-0 start-3 -translate-y-4 mt-2 h-fit bg-fundo px-0.5 text-sm font-medium text-texto transition-transform peer-placeholder-shown:-translate-y-0 peer-focus:-translate-y-4"> Mensagem </span>
                         </label>
-                        <button
-                        type="submit"
-                        class="group relative w-[25%] ml-auto cursor-pointer inline-flex items-center overflow-hidden rounded-sm bg-primaria px-8 py-3 text-texto focus:outline-hidden"
-                        >
-                            <span class="absolute -end-full transition-all group-hover:end-4">
-                                <svg
-                                class="size-5 shadow-sm rtl:rotate-180"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                                />
-                                </svg>
-                            </span>
-
-                            <span class="text-sm font-medium transition-all font-montserrat-bold group-hover:me-4"> Enviar </span>
-                        </button>
+                        <div class="justify-between flex items-center">
+                            <span class="text-texto font-montserrat text-[10px] w-[50%]">Campos com * é obrigatorio o preenchimento.</span>
+                            <button
+                            type="submit"
+                            class="group relative w-[25%] cursor-pointer inline-flex items-center overflow-hidden rounded-sm bg-primaria px-8 py-3 text-texto focus:outline-hidden"
+                            >
+                                <span class="absolute -end-full transition-all group-hover:end-4">
+                                    <svg
+                                    class="size-5 shadow-sm rtl:rotate-180"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                    />
+                                    </svg>
+                                </span>
+    
+                                <span class="text-sm font-medium transition-all font-montserrat-bold group-hover:me-4"> Enviar </span>
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -73,7 +80,17 @@
     import CampoForm from './Contato/CampoForm.vue';
 
     function enviarFormulario(){
-        const campos = document.querySelectorAll('input, textarea');
+        const campos = document.querySelectorAll('form input, form textarea');
+        campos.forEach(campo=>{
+            if(campo.value.trim() === ''){
+                alert(`O campo ${campo.name} é obrigatório.`);
+                campo.focus();
+                campo.classList.add('border-red-500');
+                return;
+            }
+            campo.classList.remove('border-red-500');
+        })
+        
         console.log(campos);
     }
 </script>
